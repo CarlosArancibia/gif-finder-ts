0594ae70c94471261c3c24a1ea7ff91e85024719
CarlosArancibia <arancibiacarlos.b@gmail.com>
2024-01-08 22:44:51 -0400

    fetch api giphy added

--- src/helpers/getGifs.ts
+++ src/helpers/getGifs.ts
@@ -1,1 +1,32 @@
-const getGifs = () => {};
+interface GifProps {
+  id: string;
+  title: string;
+  images: {
+    downsized_medium: {
+      url: string;
+    };
+  };
+}
+
+export const getGifs = async (category: string) => {
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=6KDpgrZE9eaJAVl5M2hFfnq5B552fmx3&q=${category}&limit=10`;
+
+  try {
+    const resp = await fetch(url);
+
+    if (!resp.ok) throw new Error('Error HTTP:' + resp.status);
+
+    const { data } = await resp.json();
+    const gifs = data.map(({ id, title, images }: GifProps) => {
+      return {
+        id,
+        title,
+        url: images.downsized_medium.url,
+      };
+    });
+
+    return gifs;
+  } catch (error) {
+    console.log(error);
+  }
+};
